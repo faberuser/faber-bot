272c67645b673f0a5617ba2971081c3e1027ff00
Khôi Lê <faber8164@gmail.com>
2023-05-31 15:31:56 +0700

    update docker env

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from discord import Colour
 
-# token = "MTAwMjE0ODE4NTI5MDA2Nzk5OQ.GWGowX.7Rcuxoj_8oR_MfGMq0kpqcis756IZ9fSm3yDfg" # Faber ?
-token = "ODcyMTQyNDY5NjYyMzg0MTQw.G_M81F.tdnW_XXoL213GdMmpz9xDxCwOEt4C4KgDrvtsA"  # test-bbot
+token = "MTAwMjE0ODE4NTI5MDA2Nzk5OQ.GWGowX.7Rcuxoj_8oR_MfGMq0kpqcis756IZ9fSm3yDfg" # Faber ?
+# token = "ODcyMTQyNDY5NjYyMzg0MTQw.G_M81F.tdnW_XXoL213GdMmpz9xDxCwOEt4C4KgDrvtsA"  # test-bbot
 prefix = "?"
 embed_color = Colour.blurple()
 botID = "b8d616e35e36e881"
